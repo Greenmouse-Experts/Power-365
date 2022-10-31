eff6a1d490ff576c94822d7f9dcc947cc2db4536
Promise <promiseezema11@gmail.com>
2022-10-31 11:19:36 +0100

    updated

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -18,7 +18,7 @@ class AdminSeeder extends Seeder
             'user_type' => 'Administrator',
             'first_name' => 'Admin',
             'last_name' => 'Admin',
-            'email' => 'admin@gmail.com',
+            'email' => 'support@power365es.com',
             'email_verified_at' => now(),
             'password' => bcrypt('Password1'),
             'phone_number' => '08161215848',
